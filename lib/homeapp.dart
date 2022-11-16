acceb17c9279bf3f6f686695adeb4719efc89a9c
robiul12345 <robiulstudies@gmail.com>
2022-11-17 00:57:42 +0600

    Imagec change

--- lib/homeapp.dart
+++ lib/homeapp.dart
@@ -85,145 +85,189 @@ class _MyHomeAppState extends State<MyHomeApp> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
+          backgroundColor: Colors.black,
       appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.black,
         title: Text('Wether'),
+        centerTitle: true,
       ),
       body: wetherMap != null
           ? Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(image: NetworkImage("images/ab.jpg"))),
               height: double.infinity,
               width: double.infinity,
               padding: EdgeInsets.all(4),
               child: SingleChildScrollView(
                 child: Column(
                   children: [
-                    Align(
-                      alignment: Alignment.topRight,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                    Row(children: [
+                      Expanded(
+                          flex: 3,
+                child: Container()),
+                      Expanded(
+                          flex: 7,
+                          child: Container(child:  Card(
+                        child: Align(
+                          alignment: Alignment.topCenter,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                  height:
+                                  MediaQuery.of(context).size.height * 0.01),
+                              Text(
+                                "${Jiffy(DateTime.now()).format("MMM do yy, h:mm:a")}",
+                                style: TextStyle(fontSize: 20),
+                              ),
+                              SizedBox(
+                                  height:
+                                  MediaQuery.of(context).size.height * 0.01),
+                              Text(
+                                "${wetherMap!["name"]}",
+                                style: TextStyle(fontSize: 16),
+                              ),
+                              SizedBox(
+                                  height:
+                                  MediaQuery.of(context).size.height * 0.01),
+                            ],
+                          ),
+                        ),
+                      ),)),
+                    ],),
+
+
+                    Container(
+
+                      // decoration: BoxDecoration(
+                      //   color: Colors.lightGreenAccent,
+                      // ),
+                      // // padding: EdgeInsets.all(10.0),
+                      // margin:
+                      //     EdgeInsets.symmetric(vertical: 7, horizontal: 10),
+                      child: Stack(
                         children: [
-                          SizedBox(
-                              height:
-                                  MediaQuery.of(context).size.height * 0.01),
-                          Text(
-                            "${Jiffy(DateTime.now()).format("MMM do yy, h:mm:a")}",
-                            style: TextStyle(fontSize: 20),
-                          ),
-                          SizedBox(
-                              height:
-                                  MediaQuery.of(context).size.height * 0.01),
-                          Text(
-                            "${wetherMap!["name"]}",
-                            style: TextStyle(fontSize: 16),
-                          ),
+                          Image.asset("images/a.jpg"),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                  height: MediaQuery.of(context).size.height *
+                                      0.01),
+                              Container(
+                                decoration:
+                                    BoxDecoration(shape: BoxShape.circle),
+                                child: CircleAvatar(
+                                  maxRadius: 40,
+                                  backgroundImage: NetworkImage(
+                                      "http://openweathermap.org/img/wn/10d@2x.png"),
+                                ),
+                              ),
+                              Text(
+                                "${wetherMap!["main"]["temp"]},",
+                                style: TextStyle(
+                                    fontSize: 22, color: Colors.white),
+                              ),
+                              SizedBox(
+                                  height: MediaQuery.of(context).size.height *
+                                      0.01),
+                              Column(
+                                children: [
+                                  Text(
+                                    "Feels like ${wetherMap!["main"]["feels_like"]}",
+                                    style: TextStyle(
+                                        fontSize: 20, color: Colors.white),
+                                  ),
+                                  SizedBox(
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.01),
+                                  Text(
+                                    " ${wetherMap!["weather"][0]["description"]}",
+                                    style: TextStyle(
+                                        fontSize: 20, color: Colors.white),
+                                  ),
+                                  SizedBox(
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.01),
+                                ],
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "Humidity ${wetherMap!["main"]["humidity"]},Pressure ${wetherMap!["main"]["pressure"]}",
+                                    style: TextStyle(
+                                        fontSize: 20, color: Colors.white),
+                                  ),
+                                  SizedBox(
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.02),
+                                  Text(
+                                    "Sunrise ${Jiffy(DateTime.fromMillisecondsSinceEpoch(wetherMap!["sys"]["sunrise"] * 1000)).format("h:mm:a")}, Sunset ${Jiffy(DateTime.fromMillisecondsSinceEpoch(wetherMap!["sys"]["sunset"] * 1000)).format("h:mm:a")}",
+                                    style: TextStyle(
+                                        fontSize: 16, color: Colors.black),
+                                  ),
+                                  // SizedBox(
+                                  //     height: MediaQuery.of(context).size.height *
+                                  //         0.01),
+                                ],
+                              )
+                            ],
+                          )
                         ],
                       ),
                     ),
-                    Card(
-                      elevation: 4,
-                      child: Container(
-                        width: double.infinity,
-                        padding: EdgeInsets.all(18.0),
-                        margin:
-                            EdgeInsets.symmetric(vertical: 15, horizontal: 30),
-                        child: Column(
-                          children: [
-                            SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.01),
-                            CircleAvatar(
-                              maxRadius: 40,
-                              backgroundImage: NetworkImage(
-                                  "http://openweathermap.org/img/wn/10d@2x.png"),
-                            ),
-                            Text(
-                              "${wetherMap!["main"]["temp"]}",
-                              style: TextStyle(fontSize: 22),
-                            ),
-                            SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * 0.01),
-                            Column(
-                              children: [
-                                Text(
-                                  "Feels like ${wetherMap!["main"]["feels_like"]}",
-                                  style: TextStyle(fontSize: 20),
-                                ),
-                                SizedBox(
-                                    height: MediaQuery.of(context).size.height *
-                                        0.01),
-                                Text(
-                                  " ${wetherMap!["weather"][0]["description"]}",
-                                  style: TextStyle(fontSize: 20),
-                                ),
-                                SizedBox(
-                                    height: MediaQuery.of(context).size.height *
-                                        0.01),
-                              ],
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  "Humidity ${wetherMap!["main"]["humidity"]},Pressure ${wetherMap!["main"]["pressure"]}",
-                                  style: TextStyle(fontSize: 20),
-                                ),
-                                SizedBox(
-                                    height: MediaQuery.of(context).size.height *
-                                        0.01),
-                                Text(
-                                  "Sunrise ${Jiffy(DateTime.fromMillisecondsSinceEpoch(wetherMap!["sys"]["sunrise"] * 1000)).format("h:mm:a")}, Sunset ${Jiffy(DateTime.fromMillisecondsSinceEpoch(wetherMap!["sys"]["sunset"] * 1000)).format("h:mm:a")}",
-                                  style: TextStyle(fontSize: 20),
-                                ),
-                                SizedBox(
-                                    height: MediaQuery.of(context).size.height *
-                                        0.01),
-                              ],
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.02),
                     SizedBox(
-                      height: 250,
+                      height: 230,
                       child: ListView.builder(
                           scrollDirection: Axis.horizontal,
                           itemCount: forecastMap?.length,
                           shrinkWrap: true,
-                          itemBuilder: (context, index) => Container(
-                                decoration: BoxDecoration(
-                                    color: Colors.amber,
-                                    borderRadius: BorderRadius.circular(8)),
-                                padding: EdgeInsets.all(14),
-                                margin: EdgeInsets.all(5),
-                                child: Column(
-                                  children: [
-                                    Text(
-                                        "${Jiffy(forecastMap!["list"][index]["dt_txt"]).format("EEE,h:mm:a")}"),
-                                    SizedBox(
-                                        height:
-                                            MediaQuery.of(context).size.height *
-                                                0.01),
-                                    Image.network(
-                                        "http://openweathermap.org/img/wn/10d@2x.png"),
-                                    SizedBox(
-                                        height:
-                                            MediaQuery.of(context).size.height *
-                                                0.01),
-                                    Text(
-                                      "${forecastMap!["list"][index]["main"]["temp_min"]} / ${forecastMap!["list"][index]["main"]["temp_max"]}",
-                                      style: TextStyle(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                    SizedBox(
-                                        height:
-                                            MediaQuery.of(context).size.height *
-                                                0.01),
-                                    Text(
-                                      "${forecastMap!["list"][index]["weather"][0]["main"]}",
-                                      style: TextStyle(fontSize: 16),
-                                    )
-                                  ],
+                          itemBuilder: (context, index) => Card(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      // color: Colors.lightGreen,
+                                      borderRadius: BorderRadius.circular(8)),
+                                  padding: EdgeInsets.all(14),
+                                  margin: EdgeInsets.all(5),
+                                  child: Column(
+                                    children: [
+                                      Text(
+                                          "${Jiffy(forecastMap!["list"][index]["dt_txt"]).format("EEE,h:mm:a")}"),
+                                      SizedBox(
+                                          height: MediaQuery.of(context)
+                                                  .size
+                                                  .height *
+                                              0.01),
+                                      Image.network(
+                                          "http://openweathermap.org/img/wn/10d@2x.png"),
+                                      SizedBox(
+                                          height: MediaQuery.of(context)
+                                                  .size
+                                                  .height *
+                                              0.01),
+                                      Text(
+                                        "${forecastMap!["list"][index]["main"]["temp_min"]} / ${forecastMap!["list"][index]["main"]["temp_max"]}",
+                                        style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      SizedBox(
+                                          height: MediaQuery.of(context)
+                                                  .size
+                                                  .height *
+                                              0.01),
+                                      Text(
+                                        "${forecastMap!["list"][index]["weather"][0]["main"]}",
+                                        style: TextStyle(fontSize: 16),
+                                      )
+                                    ],
+                                  ),
                                 ),
                               )),
                     ),
