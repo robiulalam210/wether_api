8dbcf94783ae05848b66928d150558cd1c366771
robiul12345 <robiulstudies@gmail.com>
2022-11-27 10:49:53 +0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/homeapp.dart

--- lib/splah.dart
+++ lib/splah.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:wether_api/homeapp.dart';
+
 class SplahScrren extends StatefulWidget {
   const SplahScrren({Key? key}) : super(key: key);
 
@@ -9,53 +10,55 @@ class SplahScrren extends StatefulWidget {
 }
 
 class _SplahScrrenState extends State<SplahScrren> {
-
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     Future.delayed(Duration(
-      seconds: 3,
-      
-    )).then((value){
-      Navigator.of(context).push(MaterialPageRoute(builder: (context)=>MyHomeApp()));
+      seconds: 5,
+    )).then((value) {
+      // Navigator.of(context)
+      //     .push(MaterialPageRoute(builder: (context) => MyHomeApp()));
+
+      Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context)=>MyHomeApp()), (route) => false);
     });
   }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
+    return SafeArea(
+        child: Scaffold(
       backgroundColor: Color(0xff071D31),
       body: Container(
         width: double.infinity,
         height: double.infinity,
         child: Stack(
-
-        children: [
-
-
-          Container( width: double.infinity,
-            height: double.infinity,
-            child: ClipRRect(
-                borderRadius: BorderRadius.circular(12),
-
-                child: Image.asset("images/a.jpg",fit: BoxFit.cover,)),
-          ),
-
-        SizedBox(
-          height: MediaQuery.of(context).size.height*0.02,
+          children: [
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.asset(
+                    "images/a.jpg",
+                    fit: BoxFit.cover,
+                  )),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.02,
+            ),
+            Positioned(
+                top: 100,
+                bottom: 100,
+                right: 100,
+                left: 100,
+                child: SpinKitSpinningLines(
+                  color: Colors.white,
+                  size: 139.0,
+                ))
+          ],
         ),
-        Positioned(
-          top: 100,
-            bottom: 100,
-            right: 100,
-            left: 100,
-
-            child:  SpinKitSpinningLines(
-          color: Colors.white,
-          size: 139.0,
-        ))
-
-      ],),),
+      ),
     ));
   }
 }
